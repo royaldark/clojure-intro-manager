28f38379f260dbfa9416dbe56c0a0c4f266af29e
Joe Einertson <jeinerts@umm>
2012-12-05 01:10:09 -0600

    Initial commit of Leiningen project file

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject manager "0.1-ALPHA"
+            :description "A skeleton of a Seesaw-based GUI for managing Leiningen projects. For use in the University of Minnesota Morris' Clojure intro class (forthcoming)."
+            :dependencies [[org.clojure/clojure "1.5.0-beta1"]
+                           [clj-stacktrace "0.2.5"]
+                           [seesaw "1.4.2"]]
+            :main manager.core)
