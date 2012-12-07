9c682cc488839b0d41a33fabdab2db5f4d0f3d2a
Joe Einertson <jeinerts@umm>
2012-12-06 23:56:03 -0600

    Cleaned up manager interface code

--- src/manager/core.clj
+++ src/manager/core.clj
@@ -11,14 +11,22 @@
   (-> f pack! show!)
   content)
 
-(defn make-button [text]
-  (button :text text))
+(defn make-button [desc]
+  (apply button desc))
+
+(def buttons (map make-button [[:text "New Project"]
+                               [:text "Open Project"]
+                               [:text "Run Project"]
+                               [:text "Duplicate Project"]
+                               [:text "Import Project"]
+                               [:text "Export Project"]
+                               [:text "Exit"
+                                :listen [:action (fn [e] (dispose! f))]]]))
 
 (defn -main [& args]
   (invoke-later
     (native!)
-    (def buttons (map make-button ["New Project", "Open Project", "Run Project", "Exit"]))
-    (def panel (grid-panel :rows 5 :columns 1
+    (def panel (grid-panel :rows (+ (count buttons) 1) :columns 1
                            :hgap 5 :vgap 5 :border 5
                            :items (cons (str "UMM Clojure Manager v." version) buttons)))
     (display panel)))
