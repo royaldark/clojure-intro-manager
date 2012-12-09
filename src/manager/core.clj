2e646f6a397cc8d6e378737506a198da077c471d
Joe Einertson <jeinerts@umm>
2012-12-08 19:58:34 -0600

    Refactored GUI functionality into separate namespace

--- src/manager/core.clj
+++ src/manager/core.clj
@@ -1,30 +1,10 @@
 (ns manager.core
   (:import [java.io File])
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [manager.gui :as gui])
   (:use [seesaw.core]
         [clojure.string :only [join split]]))
 
-(def version "0.1")
-
-(def f (frame :title "Clojure Manager"
-              :on-close :exit))
-
-(defn display [content]
-  (config! f :content content)
-  (-> f pack! show!)
-  content)
-
-(defn make-button [desc]
-  (apply button desc))
-
-(def buttons (map make-button [[:text "New Project"]
-                               [:text "Open Project"]
-                               [:text "Run Project"]
-                               [:text "Duplicate Project"]
-                               [:text "Import Project"]
-                               [:text "Export Project"]
-                               [:text "Exit"
-                                :listen [:action (fn [e] (dispose! f))]]]))
 
 (defn path [& dirs]
   (join (File/separator) dirs))
@@ -36,13 +16,16 @@
     (.write output default-contents)
     (.close output)))
 
+
 (defn parse-config-file [config-file]
   "")
+
 
 (defn setup-config-file [config-file]
   (when (. config-file createNewFile)
     (write-default-config config-file))
   (parse-config-file config-file))
+
 
 (defn setup-config-dir
   "Attempts to make the configuration directory.
@@ -57,9 +40,4 @@
         config-file (File. (str config-dir-str (File/separator) "config"))]
     (setup-config-dir config-dir)
     (setup-config-file config-file)
-    (invoke-later
-      (native!)
-      (def panel (grid-panel :rows (+ (count buttons) 1) :columns 1
-                             :hgap 5 :vgap 5 :border 5
-                             :items (cons (str "UMM Clojure Manager v." version) buttons)))
-      (display panel))))
+    (gui/show-main-window)))
